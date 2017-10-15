afaf6c1243d05f342bca48c7f131544e3de050a1
StefanSokic <stefan.sokic33@gmail.com>
2017-10-15 18:50:05 -0400

    cleaning up code

--- main.py
+++ main.py
@@ -29,99 +29,66 @@ except:
 # Globals
 windowWidth  = 600 # window dimensions
 windowHeight =  800
-
-factor = 1 # factor by which luminance is scaled
-filter = [1]
-apply_filter = False
-radius = []
-
-
+factor = 1  # factor by which luminance is scaled
+filter = [1] # initial filter
+apply_filter = False # only true when user pressed 'a'
+radius = [] # where the user right clicks
 
 # Image directory and pathe to image file
-
 imgDir      = 'images'
 imgFilename = 'veggies.jpg'
-
 imgPath = os.path.join( imgDir, imgFilename )
 
-
-
-# File dialog
-
+##  File dialog
 # import Tkinter, tkFileDialog
-#
 # root = Tkinter.Tk()
 # root.withdraw()
 
-#
-# def equalize(freqs, y_vals):
-#   L = len(freqs)
-#   pn = 0
-#   sum_px = 0
-#   for key in freqs.iterkeys():
-#     sum_px += freqs[key]
-#     pn += float(freqs[key]) / float(sum_px)
-#     freqs[key] = int(math.floor(L * pn))
-#   return freqs
-
-
 # Read and modify an image.
-
 def buildImage():
-
   # Read image and convert to YCbCr
-
   print imgPath
   src = Image.open( imgPath ).convert( 'YCbCr' )
   srcPixels = src.load()
-
   width  = src.size[0]
   height = src.size[1]
 
   # Set up a new, blank image of the same size
-
   dst = Image.new( 'YCbCr', (width,height) )
   dstPixels = dst.load()
-
-  # Build destination image from source image
-  #y_vals = []
-
-  # if radius != [0, 0]:
-  #   print radius
-    # i = radius[0]
-    # j = radius[1]
-    # final_y = 0
-    # for row in range(len(filter)):
-    #   for px in range(len(filter[row])):
-    #     # here we multiply the filter by each of the pixel values and add it to the final_y for each location
-    #     try:
-    #       final_y += filter[row][px] * srcPixels[i + row, j + px][0]
-    #     except IndexError:
-    #       # this could be fixed by wrapping the image instead of throwing down 0s
-    #       final_y += filter[row][px] * 0
-    # srcPixels[i, j][0] = int(final_y)
 
   y_vals = []
   for i in range(width):
     for j in range(height):
-
-      # read source pixel
-      
+      # reading the source
       y,cb,cr = srcPixels[i,j]
-      # print 'srcPixels[i,j]', srcPixels[i,j]
-      # try:
-      #   print 'srcPixels[i,j+1][0]', srcPixels[i,j+1][0]
-      # except IndexError:
-      #   print 0
 
-      if radius == [i, j] and radius[0] + 50 < i and radius[1] + 50 < j :
+      # Question 7: working with a radius
+      if radius == [i, j]:
+        # the initial radius is arbitrarily set to 100 to start
+        final_y = 0
         print radius
-        # large of a radius do we want to start with? make it 50?, declared up there?
-        # loop through the filter like before
+        for k in range(100):
+          # loop through the filter like before
+          # TODO: the filter begins in the wrong place right now
+          for row in range(len(filter)):
+            for px in range(len(filter[row])):
+              # here we multiply the filter by each of the pixel values and add it to the final_y for each location
+              try:
+                final_y += filter[row][px] * srcPixels[i + row, j + px][0]
+                print 'final_y', final_y
+              except IndexError:
+                # this could be fixed by wrapping the image instead of throwing down 0s
+                final_y += filter[row][px] * 0
+          y = int(final_y)
 
-      # ---- MODIFY PIXEL ----
-      y_vals.append(y)
+      # initial changing of brightness on mouse right/left
       y = int(factor * y)
+
+      # appending values so we can build a histogram
+      y_vals.append(y)
+
+      # Question 6: applying filter to whole image on 'a' press
       if filter != [1] and apply_filter:
         final_y = 0
         # TODO: the filter begins in the wrong place right now
@@ -133,21 +100,20 @@ def buildImage():
             except IndexError:
               # this could be fixed by wrapping the image instead of throwing down 0s
               final_y += filter[row][px] * 0
-
         y = int(final_y)
 
       # write destination pixel (while flipping the image in the vertical direction)
       dstPixels[i, height-j-1] = (y,cb,cr)
 
+  # Creating the plot that equalizes the histogram
   freqs = Counter(y_vals)  # creates a Counter object of all pixel y-values
   equal = equalize(add_missing_vals(freqs))  # creates cumulative distribution function
-
   #  for each pixel in y_vals, maps to corresponding pixel value using cumulative distribution function
   new = []
   for i in y_vals:
       new.append(equal[i])
   new_dict = Counter(new)
-
+  # comment these back to see the plot
   #  creates matplotlib histogram
   # plt.bar(new_dict.keys(), new_dict.values())
   # plt.show()
@@ -167,51 +133,37 @@ def equalize(freqs):
     L = 256
     map = {}
     current = 0
-
     for key in sorted(freqs.iterkeys()):
         pn = freqs[key]/float(num_pixels)
         current += pn
         new_val = floor((current*(L-1)))
         map[key] = new_val
-
     maximum = map[max(map)]
     normalizer = maximum/256.0
-
     # normalize histogram
     for key in map.iterkeys():
         map[key] = map[key]/float(normalizer)
-
     return map
 
 # Set up the display and draw the current image
-
 def display():
-
   # Clear window
-
   glClearColor ( 1, 1, 1, 0 )
   glClear( GL_COLOR_BUFFER_BIT )
 
   # rebuild the image
-
   img = buildImage()
-
   width  = img.size[0]
   height = img.size[1]
 
   # Find where to position lower-left corner of image
-
   baseX = (windowWidth-width)/2
   baseY = (windowHeight-height)/2
-
   glWindowPos2i( baseX, baseY )
 
   # Get pixels and draw
-
   imageData = numpy.array( list( img.getdata() ), numpy.uint8 )
-
   glDrawPixels( width, height, GL_RGB, GL_UNSIGNED_BYTE, imageData )
-
   glutSwapBuffers()
 
 def get_filter():
@@ -231,17 +183,13 @@ def get_filter():
   filter = clean_filter[::-1]
   
 # Handle keyboard input
-
 def keyboard( key, x, y ):
-
   if key == '\033': # ESC = exit
     sys.exit(0)
-
   elif key == 'l':
     path = tkFileDialog.askopenfilename( initialdir = imgDir )
     if path:
       loadImage( path )
-
   elif key == 's':
     outputPath = tkFileDialog.asksaveasfilename( initialdir = '.' )
     if outputPath:
@@ -251,10 +199,8 @@ def keyboard( key, x, y ):
   elif key == 'a':
     global apply_filter
     apply_filter = True
-
   else:
     print 'key =', key    # DO NOT REMOVE THIS LINE.  It will be used during automated marking.
-
   glutPostRedisplay()
 
 
@@ -265,95 +211,59 @@ def keyboard( key, x, y ):
 #
 # DO NOT CHANGE THE NAMES OR ARGUMENT LISTS OF THESE FUNCTIONS, as
 # they will be used in automated marking.
-
 def loadImage( path ):
-
   global imgPath
   imgPath = path
-
 def saveImage( path ):
-
   buildImage().save( path )
 
-  
-
 # Handle window reshape
-
 def reshape( newWidth, newHeight ):
-
   global windowWidth, windowHeight
-
   windowWidth  = newWidth
   windowHeight = newHeight
-
   glutPostRedisplay()
 
-
-
 # Mouse state on initial click
-
 button = None
 initX = 0
 initY = 0
 initFactor = 0
 
-
-
 # Handle mouse click/unclick
-
 def mouse( btn, state, x, y ):
-
   global button, initX, initY, initFactor
-
   if state == GLUT_DOWN:
-
     button = btn
     if button == 2 and filter != [1]:
       global radius
       radius = [x,y]
       glutPostRedisplay()
-
     initX = x
     initY = y
     initFactor = factor
-
   elif state == GLUT_UP:
-
     button = None
 
-
-
 # Handle mouse motion
-
 def motion( x, y ):
-
   diffX = x - initX
   diffY = y - initY
-
   global factor
-
   factor = initFactor + diffX / float(windowWidth)
-
   if factor < 0:
     factor = 0
-
   glutPostRedisplay()
-  
 
-    
 # Run OpenGL
-
 glutInit()
 glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGB )
 glutInitWindowSize( windowWidth, windowHeight )
 glutInitWindowPosition( 50, 50 )
-
 glutCreateWindow( 'imaging' )
-
 glutDisplayFunc( display )
 glutKeyboardFunc( keyboard )
 glutReshapeFunc( reshape )
 glutMouseFunc( mouse )
 glutMotionFunc( motion )
-
 glutMainLoop()
